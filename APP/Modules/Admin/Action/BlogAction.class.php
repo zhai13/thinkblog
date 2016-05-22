5767b8ac301dd4b17d2eb94d025ee36831a560f7
翟十三 <4254001@qq.com>
2016-05-22 21:24:26 +0800

    9.博文发布整合baidu编辑器
    create table thinkblog_blog(id int unsigned not null primary key
    auto_increment, title varchar(30) not null default '', content text,
    time int(10) unsigned not null datault 0, cid int unsigned not null, del
    tinyint(1) unsigned not null default 0);
    
    create table thinkblog_blog_attr(bid int unsigned not null, aid int
    unsigned not null, index bid(bid), index aid(aid));
    
    alter table thinkblog_blog add index cid(cid);

---
+++ APP/Modules/Admin/Action/BlogAction.class.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/5/17
+ * Time: 10:39
+ */
+Class BlogAction extends CommonAction {
+    //博文列表
+    Public function index() {
+        $this->display();
+    }
+
+    //添加博文
+    Public function blog() {
+        //博文所属分类
+        import('Class.Category', APP_PATH);
+        $cate = M('cate')->order('sort')->select();
+        $this->cate = Category::unlimitedForLevel($cate);
+        //博文属性
+        $this->attr = M('attr')->select();
+        $this->display();
+    }
+
+    //添加博文表单处理
+    Public function addBlog() {
+        p($_POST);
+    }
+    //编辑器图片上传处理
+    Public function upload() {
+        import('ORG.Net.UploadFile');
+        $upload = new UploadFile();
+        $upload->autoSub = true;
+        $upload->subType = 'date';
+        $upload->dateFormat = 'Ym';
+        if ($upload->upload('./Uploads/')) {
+            $info = $upload->getUploadFileInfo();
+            echo json_encode(array(
+                'url' => $info[0]['savename'],
+                'title' => htmlspecialchars($_POST['pictitle'], ENT_QUOTES),
+                'original' => $info[0]['name'],
+                'state' => 'SUCCESS',
+            ));
+        } else {
+            echo json_encode(array(
+                'state' => $upload->getErrorMsg()
+            ));
+
+        }
+    }
+}
